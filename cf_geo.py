9147bc31dd9c58337bd72d5741dbeaa8d67bb5ef
Chris <chris@csfweb.net>
2013-04-24 10:41:58 -0300

    Update cf_geo.py
    added bearing function

--- cf_geo.py
+++ cf_geo.py
@@ -15,5 +15,20 @@ def distanceKM(origin, destination):
     d = radius * c
 
     return d
+    
+def bearing(origin, destination):
+    lat1, lon1 = origin
+    lat2, lon2 = destination
+ 
+    rlat1 = math.radians(lat1)
+    rlat2 = math.radians(lat2)
+    rlon1 = math.radians(lon1)
+    rlon2 = math.radians(lon2)
+    dlon = math.radians(lon2-lon1)
+ 
+    b = math.atan2(math.sin(dlon)*math.cos(rlat2),math.cos(rlat1)*math.sin(rlat2)-math.sin(rlat1)*math.cos(rlat2)*math.cos(dlon)) # bearing calc
+    bd = math.degrees(b)
+    br,bn = divmod(bd+360,360) # the bearing remainder and final bearing
+    return bn
 
 

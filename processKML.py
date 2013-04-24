69e7397b3ced18ef43b43f739d6614a68edc7651
Chris <chris@csfweb.net>
2013-04-24 12:24:56 -0400

    update

--- processKML.py
+++ processKML.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from cf_geo import distanceKM
+from cf_geo import bearing
 
 sys.path.insert(0, '/pyxml')
 
@@ -18,13 +19,21 @@ for txt in coords:
     if not txt.startswith('0') and not txt.startswith('<'):
         txt = txt.rstrip()
         llastr = txt.split(',')
-        lla = [float(llastr[1]),float(llastr[0]),float(llastr[2])]
+        lat = float(llastr[1])
+        lon = float(llastr[0])
+        alt = float(llastr[2])
+        lla = [round(lat,6),round(lon,6),round(alt,6)]
         dist = 0
-        bearing = 0
+        heading = 0
         if lastLLA is not None:
-            dist =  distanceKM([lastLLA[0],lastLLA[1]],[lla[0],lla[1]])
-            bearing = bearing([lastLLA[0],lastLLA[1]],[lla[0],lla[1]])
-        print "Dist: " + str(dist) + " Bearing: " + str(bearing) + " Lat: " + llastr[1] + " Lon: " + llastr[0] + " Alt: " + llastr[2]
+            dist =  round(distanceKM([lastLLA[0],lastLLA[1]],[lla[0],lla[1]]),2)
+            heading = round(bearing([lastLLA[0],lastLLA[1]],[lla[0],lla[1]]),2)
+        
+        location = "&Location=" + str(lla[0]) + "," + str(lla[1])
+        head = "&heading=" + str(heading)
+        
+        print "http://maps.googleapis.com/maps/api/streetview?size=300x200" + location + head + "&sensor=false&key=AIzaSyBqftMUNYnTPUaNRoovD9AfCeHyye8H6lk"
+        #print "Dist: " + str(dist) + " Bearing: " + str(heading) + " Location=" + llastr[1] + "," + llastr[0] + " Alt: " + llastr[2]
         lastLLA = lla
     i+=1
 

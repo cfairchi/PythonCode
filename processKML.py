07cb762301f196d53c07debe639daeb0432756f8
Chris Fairchild <chris@csfweb.net>
2013-04-25 04:31:06 -0400

    update

--- processKML.py
+++ processKML.py
@@ -1,5 +1,10 @@
 import sys
 import os
+import urllib
+import cv
+import Image
+
+
 from cf_geo import distanceKM
 from cf_geo import bearing
 
@@ -13,6 +18,10 @@ content = lines.split('<LineString>')
 allcoords = str(content[1])
 coords = allcoords.split(' ')
 i = 0
+
+
+fourcc = cv.CV_FOURCC('M','P','1','V')
+w = cv.CreateVideoWriter("testvideo.avi", fourcc, 30,(800,600), is_color=1)
 
 lastLLA = None
 for txt in coords:
@@ -29,14 +38,20 @@ for txt in coords:
             dist =  round(distanceKM([lastLLA[0],lastLLA[1]],[lla[0],lla[1]]),2)
             heading = round(bearing([lastLLA[0],lastLLA[1]],[lla[0],lla[1]]),2)
         
-        location = "&Location=" + str(lla[0]) + "," + str(lla[1])
-        head = "&heading=" + str(heading)
-        
-        print "http://maps.googleapis.com/maps/api/streetview?size=300x200" + location + head + "&sensor=false"
+        location = "&location=" + str(lla[0]) + ",%20" + str(lla[1])
+        head = "&heading=" + str(int(heading))
+
+        urllib.urlretrieve("http://maps.googleapis.com/maps/api/streetview?size=400x400" + location + "&fov=90" + head + "&pitch=10&sensor=false", "sv_" + str(i) +".jpg")
+        print "http://maps.googleapis.com/maps/api/streetview?size=400x400" + location + "&fov=90" + head + "&pitch=10&sensor=false"
+        im = Image.open("sv_" + str(i) +".jpg")
+        im.save("sv_" + str(i) +".png")
+        #cv.WriteFrame(w, "sv_" + str(i) +".png")
+
         #print "Dist: " + str(dist) + " Bearing: " + str(heading) + " Location=" + llastr[1] + "," + llastr[0] + " Alt: " + llastr[2]
         lastLLA = lla
     i+=1
 
  # doc = parser.parse(f)
 
-
+print "http://maps.googleapis.com/maps/api/streetview?size=400x400&location=38.14842,%20-107.81027&fov=90&heading=235&pitch=10&sensor=false"
+print "http://maps.googleapis.com/maps/api/streetview?size=400x400&Location=38.14842,%20-107.81027&fov=90&heading=235&pitch=10&sensor=false"

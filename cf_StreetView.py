463dc57ae5977e74094082de253b1f8e0c0dbc74
Chris Fairchild <chris@csfweb.net>
2013-04-28 16:19:04 -0400

    updated

--- cf_StreetView.py
+++ cf_StreetView.py
@@ -1,8 +1,9 @@
 import sys
 import os
 import urllib
-#import cv
-#import Image
+import cv
+import Image
+import time
 
 from cf_geo import distanceKM
 from cf_geo import bearing
@@ -12,8 +13,9 @@ def generateVideoFromKML(thekml, theVideoFileName):
     allcoords = str(content[1])
     coords = allcoords.split(' ')
     i = 0
-    #fourcc = cv.CV_FOURCC('M','P','1','V')
-    #w = cv.CreateVideoWriter(theVideoFileName + ".avi", fourcc, 30,(400,400), is_color=1)
+    fourcc = cv.CV_FOURCC('P','I','M','1')
+    outFile = theVideoFileName + ".mpeg"
+    w = cv.CreateVideoWriter(outFile, fourcc, 24,(400,300), is_color=1)
 
     lastLLA = None
     
@@ -29,19 +31,27 @@ def generateVideoFromKML(thekml, theVideoFileName):
             heading = 0
             if lastLLA is not None:
                 dist =  round(distanceKM([lastLLA[0],lastLLA[1]],[lla[0],lla[1]]),2)
-                heading = round(bearing([lastLLA[0],lastLLA[1]],[lla[0],lla[1]]),2)
+                heading = int(bearing([lastLLA[0],lastLLA[1]],[lla[0],lla[1]]))
             
             location = "&location=" + str(lla[0]) + ",%20" + str(lla[1])
             head = "&heading=" + str(heading)
             
-            urlString = "http://maps.googleapis.com/maps/api/streetview?size=400x300" + location + "&fov=90" + head + "&pitch=10&sensor=false"
-            #urllib.urlretrieve(urlString, "sv_" + str(i) +".jpg")
-            print urlString
-            #img = cv.LoadImage("sv_" + str(i) +".jpg")
-            #im.save("sv_" + str(i) +".png")
-            #cv.WriteFrame(w, img)
-            #os.remove("sv_" + str(i) +".jpg")
+            urlString = "http://maps.googleapis.com/maps/api/streetview?size=400x300" + location + "&fov=90" + head + "&pitch=10&sensor=false&key=AIzaSyBqftMUNYnTPUaNRoovD9AfCeHyye8H6lk"
+            urllib.urlretrieve(urlString, "sv_" + str(i) +".jpg")
+            statinfo = os.stat("sv_" + str(i) +".jpg")
+            #print statinfo.st_size
+            if (statinfo.st_size > 5000):
+                #print urlString
+                img = cv.LoadImage("sv_" + str(i) +".jpg")
+                #im.save("sv_" + str(i) +".png")
+                for x in range(0, 5):
+                    cv.WriteFrame(w, img)
+
+            os.remove("sv_" + str(i) +".jpg")
             lastLLA = lla
+            #if i >50: break
+            #time.sleep(1)
+            print i
             i+=1
     print str(i) + " Images Processed"
     

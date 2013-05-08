9bb30a5c1f4baf349de9308365910e0892d76223
Chris Fairchild <chris@fairchildsystems.com>
2013-05-08 10:14:57 -0400

    update

--- pyDBLib/test.py
+++ pyDBLib/test.py
@@ -6,35 +6,7 @@ import sys
 import DBObject
 from DBDrive import DBDrive
 
-def createTable(theDBObject,theDatabase,theDeleteIfExists):
-	con = None
-	try:
-		con = lite.connect('BywayExplorer.db')
-		cur = con.cursor()
- 		if (theDeleteIfExists):
-			cur.execute("DROP TABLE IF EXISTS " + theDBObject.getTableName())
-		sqlCmd = "CREATE TABLE "
-		sqlCmd = sqlCmd + theDBObject.getTableName() + "("
-		i = 0
-		colNames = theDBObject.getColumns()
-		for col in colNames:
-			if (i !=0 ):
-				sqlCmd = sqlCmd + "," + col + " TEXT"
-			else:
-				sqlCmd = sqlCmd + col + " TEXT"
-		sqlCmd = sqlCmd + ")"
-		cur.execute(sqlCmd)
-		print("Table " + theDBObject.getTableName() + " Created")
-		con.commit()
-	except lite.Error, e:
-		if con:
-			con.rollback()
-			print "Error %s:" % e.args[0]
-			sys.exit(1)
-	finally:
-		if con:
-			con.close()
 		
 drive = DBDrive()
-createTable(drive,"BywayExplorer.db",True)
+drive.createTable("BywayExplorer.db",true)
 

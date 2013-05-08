08e8a2f50acea428e50a3af214fd43dd2368170b
Chris Fairchild <chris@fairchildsystems.com>
2013-05-08 11:37:58 -0400

    tree

--- pyDBLib/test.py
+++ pyDBLib/test.py
@@ -5,9 +5,14 @@ import sqlite3 as lite
 import sys
 import DBObject
 from DBDrive import DBDrive
-import sqliteDBUtils 
-
+import xml.etree.ElementTree as ET
 		
 drive = DBDrive()
 drive.createSQLiteTable("BywayExplorer.db",True)
-
+tree = ET.parse("byways.xml")
+elem = tree.getroot()
+byways = elem.findall("Byway")
+for byway in byways:
+	drive = DBDrive()
+	drive.id = byway.find("id").text
+	drive.

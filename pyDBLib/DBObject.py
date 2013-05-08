09ef48b6ad2ee62791fa1ce11356d9541af7690f
Chris <chris@csfweb.net>
2013-05-08 11:40:42 -0300

    Update DBObject.py

--- pyDBLib/DBObject.py
+++ pyDBLib/DBObject.py
@@ -11,30 +11,3 @@ class DBObject(object):
  	def getTableName(self):
  		raise NotImplementedError, "Please override in derived class" 		
 
-	def __createSQLiteTable__(theSQLiteDatabaseName,theDeleteIfExists):
-		con = None
-		try:
-			con = lite.connect(theSQLiteDatabaseName)
-			cur = con.cursor()
-			if (theDeleteIfExists):
-				cur.execute("DROP TABLE IF EXISTS " + getTableName())
-			sqlCmd = "CREATE TABLE " + getTableName() + "("
-			i = 0
-			colNames = getColumns()
-			for col in colNames:
-				if (i !=0 ):
-					sqlCmd = sqlCmd + "," + col + " TEXT"
-				else:
-					sqlCmd = sqlCmd + col + " TEXT"
-			sqlCmd = sqlCmd + ")"
-			cur.execute(sqlCmd)
-			print("Table " + getTableName() + " Created")
-			con.commit()
-		except lite.Error, e:
-			if con:
-				con.rollback()
-				print "Error %s:" % e.args[0]
-				sys.exit(1)
-		finally:
-			if con:
-				con.close()

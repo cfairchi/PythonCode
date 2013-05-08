070f3dece322cbef33b381252379bfd54ef1efa6
Chris <chris@csfweb.net>
2013-05-08 14:24:05 -0300

    Update test.py

--- pyDBLib/test.py
+++ pyDBLib/test.py
@@ -7,6 +7,7 @@ import DBObject
 from DBDrive import DBDrive
 import xml.etree.ElementTree as ET
 		
+
 drive = DBDrive()
 drive.createSQLiteTable("BywayExplorer.db",True)
 tree = ET.parse("byways.xml")
@@ -14,27 +15,34 @@ elem = tree.getroot()
 byways = elem.findall("Byway")
 for byway in byways:
     drive = DBDrive()
-    drive.driveid = byway.find("id").text
-    drive.driveName = byway.find("Name").text
+    if (byway.find("id") isnot None):
+    	drive.driveid = byway.find("id").text
+    if (byway.find("Name") isnot None):
+    	drive.driveName = byway.find("Name").text
     drive.country = "US"
-    drive.region = byway.find("States").find("State").text
+    if (byway.find("States") isnot None):
+    	if (byway.find("State") isnot None):
+    		drive.region = byway.find("States").find("State").text
     drive.startLat = ""
     drive.startLon = ""
     drive.stopLat = ""
     drive.stopLon = ""
     drive.description = byway.find("ShortDescription").text + byway.find("FullDescription").text
-    drive.mileage = byway.find("Length").text
-    drive.timeToAllow = byway.find("SuggestedTime").text
+    if (byway.find("Length") isnot None):
+    	drive.mileage = byway.find("Length").text
+    if (byway.find("SuggestedTime") isnot None):
+    	drive.timeToAllow = byway.find("SuggestedTime").text
     drive.favorite = "false"
     if (byway.find("Photo") is not None):
     	if (byway.find("Photo").find("URL") is not None):
     		drive.image = byway.find("Photo").find("URL").text
-    		
-    drive.imageCredits = byway.find("Photo").find("Credits").text
+    	if (byway.find("Photo").find("Credits") is not None):
+    		drive.imageCredits = byway.find("Photo").find("Credits").text
     drive.mapimage = ""
     drive.restrictions = ""
     drive.seasons = ""
     drive.considerations = ""
     drive.directions = ""
-    drive.coordinates = byway.find("Route")
+    if (byway.find("Route") isnot None):
+    	drive.coordinates = byway.find("Route")
     drive.toString()

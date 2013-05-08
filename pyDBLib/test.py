2bfe0169c154dd330d3f4a2c84dbc00decd85d69
Chris <chris@csfweb.net>
2013-05-08 14:53:00 -0300

    Update test.py

--- pyDBLib/test.py
+++ pyDBLib/test.py
@@ -17,11 +17,11 @@ for byway in byways:
     drive = DBDrive()
     if (byway.find("id") is not None and byway.find("id").text is not None):
     	drive.driveid = byway.find("id").text
-    if (byway.find("Name").text is not None):
+    if (byway.find("Name") is not None and byway.find("Name").text is not None):
     	drive.driveName = byway.find("Name").text
     drive.country = "US"
     if (byway.find("States") is not None):
-    	if (byway.find("State").text is not None):
+    	if (byway.find("State") is not None and byway.find("State").text is not None):
     		drive.region = byway.find("States").find("State").text
     drive.startLat = ""
     drive.startLon = ""
@@ -29,22 +29,22 @@ for byway in byways:
     drive.stopLon = ""
     sd = ""
     fd = ""
-    if (byway.find("ShortDescription").text is not None):
+    if (byway.find("ShortDescription") is not None and byway.find("ShortDescription").text is not None):
     	sd = byway.find("ShortDescription").text
     	
-    if (byway.find("FullDescription").text is not None):
+    if (byway.find("FullDescription") is not None and byway.find("FullDescription").text is not None):
     	fd = byway.find("FullDescription").text
     	
     drive.description = sd + fd
-    if (byway.find("Length").text is not None):
+    if (byway.find("Length") is not None and byway.find("Length").text is not None):
     	drive.mileage = byway.find("Length").text
-    if (byway.find("SuggestedTime").text is not None):
+    if (byway.find("SuggestedTime") is not None and byway.find("SuggestedTime").text is not None):
     	drive.timeToAllow = byway.find("SuggestedTime").text
     drive.favorite = "false"
     if (byway.find("Photo") is not None):
-    	if (byway.find("Photo").find("URL").text is not None):
+    	if (byway.find("Photo").find("URL") is not None and byway.find("Photo").find("URL").text is not None):
     		drive.image = byway.find("Photo").find("URL").text
-    	if (byway.find("Photo").find("Credits").text is not None):
+    	if (byway.find("Photo").find("Credits") is not None and byway.find("Photo").find("Credits").text is not None):
     		drive.imageCredits = byway.find("Photo").find("Credits").text
     drive.mapimage = ""
     drive.restrictions = ""

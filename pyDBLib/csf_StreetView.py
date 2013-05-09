eb96e03515694c26d57e007fed31c34f47b84296
Chris Fairchild <chris@fairchildsystems.com>
2013-05-09 17:55:19 -0400

    images

---
+++ pyDBLib/csf_StreetView.py
@@ -0,0 +1,59 @@
+import sys
+import os
+import urllib
+import cv
+import Image
+import time
+
+from cf_geo import distanceKM
+from cf_geo import bearing
+from GoogleAPIKey import getGoogleAPIKey
+
+def generateVideoFromCoords(theCoords, theVideoFileName):
+    i = 0
+    fourcc = cv.CV_FOURCC('P','I','M','1')
+    outFile = theVideoFileName + ".mpeg"
+    w = cv.CreateVideoWriter(outFile, fourcc, 24,(300,300), is_color=1)
+
+    lastLLA = None
+    
+    for txt in coords:
+        if not txt.startswith('0') and not txt.startswith('<'):
+            txt = txt.rstrip()
+            llastr = txt.split(',')
+            lat = float(llastr[1])
+            lon = float(llastr[0])
+            alt = float(llastr[2])
+            lla = [round(lat,6),round(lon,6),round(alt,6)]
+            dist = 0
+            heading = 0
+            if lastLLA is not None:
+                dist =  round(distanceKM([lastLLA[0],lastLLA[1]],[lla[0],lla[1]]),2)
+                heading = int(bearing([lastLLA[0],lastLLA[1]],[lla[0],lla[1]]))
+            
+            location = "&location=" + str(lla[0]) + ",%20" + str(lla[1])
+            head = "&heading=" + str(heading)
+            
+            urlString = "http://maps.googleapis.com/maps/api/streetview?size=300x300" + location + "&fov=90" + head + "&pitch=10&sensor=false" + getGoogleAPIKey()
+            urllib.urlretrieve(urlString, "sv_" + str(i) +".jpg")
+            statinfo = os.stat("sv_" + str(i) +".jpg")
+            if (statinfo.st_size > 5000):
+                img = cv.LoadImage("sv_" + str(i) +".jpg")
+                for x in range(0, 8):
+                    cv.WriteFrame(w, img)
+
+            os.remove("sv_" + str(i) +".jpg")
+            lastLLA = lla
+            print i
+            i+=1
+    print str(i) + " Images Processed"
+    
+    
+def generateVideoFromKML(thekml, theVideoFileName):
+    content = thekml.split('<LineString>')
+    allcoords = str(content[1])
+    coords = allcoords.split(' ')
+    generateVideoFromCoords(coords,theVideoFileName)
+    
+    
+        

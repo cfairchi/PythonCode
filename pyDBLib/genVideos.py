68fae6bc04e332114b5e8bbd0ba3301b8710aa59
Chris <chris@csfweb.net>
2013-05-09 12:44:10 -0300

    Update genVideos.py

--- pyDBLib/genVideos.py
+++ pyDBLib/genVideos.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# _*_ coding: utf-8 _*_
+
 import sys
 import os
 import sqlite3
@@ -8,13 +11,15 @@ import DBCoordinate from DBCoordinate
 from csf_StreetView import generateVideoFromCoords
 
 try:
+  totalRequests = 0
   con = sqlite3.connect('BywayExplorer.db")
   cur = con.cursor()
   cur.execute("SELECT driveid FROM " + DBDrive.getTableName())
   driveRows = cur.fetchall()
   for driveRow in driveRows:
     driveid = driveRow[0]
-    if (not os.path.exists(driveid + "_video")):
+    outFile = driveid + "_video")
+    if (not os.path.exists(outFile)):
       coordList = []
       print ("SELECT * FROM " + DBCoordinate.getTableName() " + " WHERE driveid='" + driveid + "'")
       cur.execute("SELECT * FROM " + DBCoordinate.getTableName() " + " WHERE driveid='" + driveid + "'")
@@ -24,6 +29,13 @@ try:
         lat = coord[2]
         lon = coord[3]
         coordList.append(lat + "," + lon + "," + 0)
+        totalRequests = totalRequests + 1
+      
+      print( "Generating: " + outFile)
+      generateVideoFromCoords(coordList,driveid + "_video")
+      print( "Done Generating: " + outFile + " Total Requests:" + totalRequests)
+    else:
+      print( "File Already Exists: " + outFile)
         
       
     

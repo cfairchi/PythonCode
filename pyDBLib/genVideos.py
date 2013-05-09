9f72df7900a93c8b5561706b53ac4d0ac5316ce5
Chris <chris@csfweb.net>
2013-05-09 12:14:30 -0300

    Update genVideos.py

--- pyDBLib/genVideos.py
+++ pyDBLib/genVideos.py
@@ -2,6 +2,10 @@ import sys
 import sqlite3
 from csf_StreetView import generateVideoFromCoords
 
-
+try:
+  con = sqlite3.connect('BywayExplorer.db")
+  cur = con.cursor()
+  cur.execute("SELECT * FROM " + DBDrive.g
+  
 
 

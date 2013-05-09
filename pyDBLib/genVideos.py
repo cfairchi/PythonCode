6aff147fd8acafd379220879d31ebdba785c84ed
Chris <chris@csfweb.net>
2013-05-09 12:36:28 -0300

    Update genVideos.py

--- pyDBLib/genVideos.py
+++ pyDBLib/genVideos.py
@@ -4,6 +4,7 @@ import sqlite3
 import DBDrive from DBDrive
 import DBCoordinate from DBCoordinate
 
+
 from csf_StreetView import generateVideoFromCoords
 
 try:
@@ -13,14 +14,18 @@ try:
   driveRows = cur.fetchall()
   for driveRow in driveRows:
     driveid = driveRow[0]
-    print ("SELECT * FROM " + DBCoordinate.getTableName() " + " WHERE driveid='" + driveid + "'")
-    cur.execute("SELECT * FROM " + DBCoordinate.getTableName() " + " WHERE driveid='" + driveid + "'")
-    coords = cur.fetchall()
+    if (not os.path.exists(driveid + "_video")):
+      coordList = []
+      print ("SELECT * FROM " + DBCoordinate.getTableName() " + " WHERE driveid='" + driveid + "'")
+      cur.execute("SELECT * FROM " + DBCoordinate.getTableName() " + " WHERE driveid='" + driveid + "'")
+      coords = cur.fetchall()
     
-    for coord in coords:
-      lat = coord[2]
-      lon = coord[3]
-      alt = 0
+      for coord in coords:
+        lat = coord[2]
+        lon = coord[3]
+        coordList.append(lat + "," + lon + "," + 0)
+        
+      
     
   
 

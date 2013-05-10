5956a9cdb753220d42276ce517d6c50f1a526511
Chris Fairchild <chris@fairchildsystems.com>
2013-05-10 13:26:36 -0400

    all

--- pyDBLib/deleteDupeCoords.py
+++ pyDBLib/deleteDupeCoords.py
@@ -9,7 +9,7 @@ from DBDrive import DBDrive
 from chrispwd import getUserName
 from chrispwd import getPassword
 
-def getMySqlConnection(self, theDBName):
+def getMySqlConnection(theDBName):
   return MySQLdb.Connection(user=getUserName(), passwd=getPassword(), db=theDBName, host='localhost')
 
 con = getMySqlConnection("djangosite")
@@ -17,18 +17,16 @@ con = getMySqlConnection("djangosite")
 try:
   coords = []
   cur = con.cursor(MySQLdb.cursors.DictCursor)
-  cur.execute("SELECT * from FROM bywayexplorer_coordinate ORDER BY driveid")
+  cur.execute("SELECT * FROM bywayexplorer_coordinate ORDER BY driveid")
   coordinates = cur.fetchall()
   i = 0
   for coord in coordinates:
-    coordString = coord["driveid"]+coord["latitude"]+coord["longitude"]
+    coordString = coord["driveid"]+str(coord["latitude"])+str(coord["longitude"])
     if (coordString in coords):
-      print("Duplicate: " + coord[id] + "," + coord["driveid"] + "," + coord["latitude"] + "," + coord["longitude"])
+      i+=1
+      print("Duplicate:(" + str(i) + ") " + str(coord["id"]) + "," + coord["driveid"] + "," + str(coord["latitude"]) + "," + str(coord["longitude"]))
     else:
       coords.append(coordString)
-    i += 1
-    if (i > 5):
-      break
 except MySQLdb.Error, e:
   print "Error %d: %s" % (e.args[0],e.args[1])
   sys.exit(1)

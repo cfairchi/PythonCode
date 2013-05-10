adcbf634b1577993e2fcf55bbdaecdf28e7665e7
Chris <chris@csfweb.net>
2013-05-10 09:35:34 -0300

    Create copyCoords.py

---
+++ pyDBLib/copyCoords.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# _*_ coding: utf-8 _*_
+
+import sys
+import os
+import sqlite3
+from DBDrive import DBDrive
+from DBCoordinate import DBCoordinate
+
+con = sqlite3.connect("BywayExplorer.db")
+try:
+  totalRequests = 0
+  cur = con.cursor()
+  drive = DBDrive()
+  cur.execute("SELECT * FROM " + drive.getTableName())
+  driveRows = cur.fetchall()
+  print(len(driveRows))
+  for driveRow in driveRows:
+    driveid = driveRow[0]
+    newDrive = DBDrive()
+    newDrive.driveid = driveRow[0]
+    newDrive.driveName = driveRow[1].replace("'","''")
+    newDrive.country = driveRow[2]
+    newDrive.region = driveRow[3]
+    newDrive.startLat = driveRow[4]
+    newDrive.startLon = driveRow[5]
+    newDrive.stopLat = driveRow[6]
+    newDrive.stopLon = driveRow[7]
+    newDrive.shortDescription = ""
+    newDrive.longDescription = driveRow[8].replace("'","''")
+    newDrive.mileage = driveRow[9].replace("'","''")
+    newDrive.timeToAllow = driveRow[10].replace("'","''")
+    newDrive.favorite = driveRow[11]
+    newDrive.image = driveRow[12].replace("'","''")
+    newDrive.mapimage = driveRow[13]
+    newDrive.restrictions = driveRow[14].replace("'","''")
+    newDrive.seasons = driveRow[15].replace("'","''")
+    newDrive.considerations = driveRow[16].replace("'","''")
+    newDrive.directions = driveRow[17].replace("'","''")
+    newDrive.imageCredits = driveRow[18].replace("'","''")
+    newDrive.insertIntoMySQLDB("djangosite","bywayexplorer_drive")
+except sqlite3.Error, e:
+  print "Error %d: %s" % (e.args[0],e.args[1])
+  sys.exit(1)
+finally:
+  if con:
+    con.close() 

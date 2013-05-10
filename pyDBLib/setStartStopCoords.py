031b777b4cf308936f4115d6ba456974fd076484
Chris Fairchild <chris@fairchildsystems.com>
2013-05-10 15:15:55 -0400

    all

--- pyDBLib/setStartStopCoords.py
+++ pyDBLib/setStartStopCoords.py
@@ -34,9 +34,9 @@ try:
     drv.stopLat = coordinates[last]["latitude"]
     drv.stopLon = coordinates[last]["longitude"]
     cur3 = con.cursor()
-    print("UPDATE bywayexplorer_drive SET startLat=" + drv.startLat + ", startLon=" + drv.startLon + ", stopLat=" + drv.stopLat + ", stopLon=" + drv.stopLon + " WHERE driveid='" + drv.driveid + "'")
-    cur3.execute("UPDATE bywayexplorer_drive SET startLat=" + drv.startLat + ", startLon=" + drv.startLon + ", stopLat=" + drv.stopLat + ", stopLon=" + drv.stopLon + " WHERE driveid='" + drv.driveid + "'"  )
-    cur3.commit
+    print("UPDATE bywayexplorer_drive SET startLat=" + str(drv.startLat) + ", startLon=" + str(drv.startLon) + ", stopLat=" + str(drv.stopLat) + ", stopLon=" + str(drv.stopLon) + " WHERE driveid='" + str(drv.driveid) + "'")
+    cur3.execute("UPDATE bywayexplorer_drive SET startLat=" + str(drv.startLat) + ", startLon=" + str(drv.startLon) + ", stopLat=" + str(drv.stopLat) + ", stopLon=" + str(drv.stopLon) + " WHERE driveid='" + str(drv.driveid) + "'"  )
+    con.commit
     
 except MySQLdb.Error, e:
   print "Error %d: %s" % (e.args[0],e.args[1])

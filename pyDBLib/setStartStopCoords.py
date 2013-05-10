544b845150592e367b9c7dfb8c1367ff3da68869
Chris <chris@csfweb.net>
2013-05-10 16:18:25 -0300

    Update setStartStopCoords.py

--- pyDBLib/setStartStopCoords.py
+++ pyDBLib/setStartStopCoords.py
@@ -13,19 +13,26 @@ from chrispwd import getPassword
 def getMySqlConnection(theDBName):
   return MySQLdb.Connection(user=getUserName(), passwd=getPassword(), db=theDBName, host='localhost')
 
-con = getMySqlConnection("djangosite")
+con1 = getMySqlConnection("djangosite")
 
 try:
   totalRequests = 0
-  cur = con.cursor(MySQLdb.cursors.DictCursor)
+  cur = con1.cursor(MySQLdb.cursors.DictCursor)
   coord = DBCoordinate()
   cur.execute("SELECT driveid FROM bywayexplorer_drive")
   driveRows = cur.fetchall()
+  if con1:
+    con1.close()
+    
   for drive in driveRows:
     driveid = drive["driveid"]
-    cur2 = con.cursor(MySQLdb.cursors.DictCursor)
+    con2 = getMySqlConnection("djangosite")
+    cur2 = con2.cursor(MySQLdb.cursors.DictCursor)
     cur2.execute("SELECT * FROM bywayexplorer_coordinate WHERE driveid = '" + driveid + "' ORDER BY routeOrder")
     coordinates = cur2.fetchall()
+    if con2:
+      con2.close()
+      
     drv = DBDrive()
     drv.driveid = coordinates[0]["driveid"]
     drv.startLat = coordinates[0]["latitude"]
@@ -33,10 +40,13 @@ try:
     last = len(coordinates) - 1 
     drv.stopLat = coordinates[last]["latitude"]
     drv.stopLon = coordinates[last]["longitude"]
-    cur3 = con.cursor()
+    con3 = getMySqlConnection("djangosite")
+    cur3 = con3.cursor()
     print("UPDATE bywayexplorer_drive SET startLat=" + str(drv.startLat) + ", startLon=" + str(drv.startLon) + ", stopLat=" + str(drv.stopLat) + ", stopLon=" + str(drv.stopLon) + " WHERE driveid='" + str(drv.driveid) + "'")
     cur3.execute("UPDATE bywayexplorer_drive SET startLat=" + str(drv.startLat) + ", startLon=" + str(drv.startLon) + ", stopLat=" + str(drv.stopLat) + ", stopLon=" + str(drv.stopLon) + " WHERE driveid='" + str(drv.driveid) + "'"  )
-    con.commit
+    con3.commit
+    if con3:
+      con3.close()
     
 except MySQLdb.Error, e:
   print "Error %d: %s" % (e.args[0],e.args[1])

9db16510c42b0c26ed563ebd99aff6ff65a4dcc2
Chris Fairchild <chris@fairchildsystems.com>
2013-05-13 18:48:48 -0400

    del coords works

--- pyDBLib/deleteDupeCoords.py
+++ pyDBLib/deleteDupeCoords.py
@@ -13,12 +13,13 @@ def getMySqlConnection(theDBName):
   return MySQLdb.Connection(user=getUserName(), passwd=getPassword(), db=theDBName, host='localhost')
 
 con = getMySqlConnection("djangosite")
-conn.autocommit(True);
+con.autocommit(True);
 
 try:
   cur = con.cursor(MySQLdb.cursors.DictCursor)
   cur.execute("SELECT driveid FROM bywayexplorer_drive")
   drives = cur.fetchall()
+  cur.close()
   i = 0
   dCount = 0
   for drive in drives:
@@ -28,14 +29,13 @@ try:
     cur = con.cursor(MySQLdb.cursors.DictCursor)
     cur.execute("SELECT * FROM bywayexplorer_coordinate WHERE driveid ='" + drive["driveid"] + "' ORDER BY routeOrder")
     coordinates = cur.fetchall()
-    
+    cur.close()
     for coord in coordinates:
-      con = getMySqlConnection("djangosite")
       coordString = coord["driveid"]+str(coord["latitude"])+str(coord["longitude"])
       if (coordString in coords):
         i+=1
         cur = con.cursor()
-        sql = "DELETE FROM bywayexplorer_coordinate WHERE id = '" + str(coord["id"]) + "';"
+        sql = "DELETE FROM bywayexplorer_coordinate WHERE id = " + str(coord["id"]) 
         print(sql)
         cur.execute(sql)
         

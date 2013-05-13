0bc90f6a6376f93e90719f3477370167897c0278
Chris Fairchild <chris@fairchildsystems.com>
2013-05-13 18:57:05 -0400

    update

--- pyDBLib/updateMySQLDB.py
+++ pyDBLib/updateMySQLDB.py
@@ -19,23 +19,20 @@ def getMySqlConnection(theDBName):
   return MySQLdb.Connection(user=getUserName(), passwd=getPassword(), db=theDBName, host='localhost')
 
 con = getMySqlConnection("djangosite")
-conn.autocommit(True);
+con.autocommit(True);
 
 try:
 	cur = con.cursor()
-	liteCon = lite.connect('BywayExplorer_First.db')
+	liteCon = lite.connect('bywayexplorer_current.db')
 	liteCur = liteCon.cursor()
-	liteCur.execute("SELECT * from bywayexplorer_drive")
+	liteCur.execute("SELECT * FROM drives")
 	liteDrives = liteCur.fetchall()
 
 	for driveRow in liteDrives:
-		short = driveRow[8]
-		longD = driveRow[9]
-
-		cur.execute("UPDATE bywayexplorer_drive SET shortDescription = %s WHERE driveid = %s", (short, driveRow[0]))    
-	    print drv.driveid + " Number of rows updated: %d" % cur.rowcount
-        cur.execute("UPDATE bywayexplorer_drive SET longDescription = %s WHERE driveid = %s", (longD, driveRow[0]))        
-        print drv.driveid + " Number of rows updated: %d" % cur.rowcount 
+		cur.execute("UPDATE bywayexplorer_drive SET region = %s WHERE driveid = %s", (driveRow[3], driveRow[0]))    
+		print driveRow[0] + " Number of rows updated: %d" % cur.rowcount
+        	cur.execute("UPDATE bywayexplorer_drive SET longDescription = %s WHERE driveid = %s", (longD, driveRow[0]))        
+        	print driveRow[0] + " Number of rows updated: %d" % cur.rowcount 
 
 except MySQLdb.Error, e:
   print "Error %d: %s" % (e.args[0],e.args[1])

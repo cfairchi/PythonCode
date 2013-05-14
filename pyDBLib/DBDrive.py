2d77a4e1e4fd90a9f3a4ebc2a8301cbbd83c4eea
Chris <chris@csfweb.net>
2013-05-14 07:28:52 -0300

    Update DBDrive.py

--- pyDBLib/DBDrive.py
+++ pyDBLib/DBDrive.py
@@ -2,50 +2,50 @@ import abc
 from DBObject import DBObject
 
 class DBDrive(DBObject):
-	driveid = ""
-	driveName = ""
-	country = ""
-	region = ""
-	startLat = ""
-	startLon = ""
-	stopLat = ""
-	stopLon = ""
-	shortDescription = ""
-	longDescription = ""
-	mileage = ""
-	timeToAllow = ""
-	favorite = ""
-	image = ""
-	mapimage = ""
-	restrictions = ""
-	seasons = ""
-	considerations = ""
-	directions = ""
-	imageCredits = ""
+	#driveid = ""
+	#driveName = ""
+	#country = ""
+	#region = ""
+	#startLat = ""
+	#startLon = ""
+	#stopLat = ""
+	#stopLon = ""
+	#shortDescription = ""
+	#longDescription = ""
+	#mileage = ""
+	#timeToAllow = ""
+	#favorite = ""
+	#image = ""
+	#mapimage = ""
+	#restrictions = ""
+	#seasons = ""
+	#considerations = ""
+	#directions = ""
+	#imageCredits = ""
     
-	def getValues(self):
-    		values = {}
-    		values["driveid"] = self.driveid
-	    	values["driveName"] = self.driveName
-		values["country"] = self.country
-		values["region"] = self.region
-		values["startLat"] = self.startLat
-		values["startLon"] = self.startLon
-		values["stopLat"] = self.stopLat
-		values["stopLon"] = self.stopLon
-		values["shortDescription"] = self.shortDescription
-		values["longDescription"] = self.longDescription
-		values["mileage"] = self.mileage
-		values["timeToAllow"] = self.timeToAllow
-		values["favorite"] = self.favorite
-		values["image"] = self.image
-		values["mapimage"] = self.mapimage
-		values["restrictions"] = self.restrictions
-		values["seasons"] = self.seasons
-		values["considerations"] = self.considerations
-		values["directions"] = self.directions
-		values["imageCredits"] = self.imageCredits
-    		return values
+	#def getValues(self):
+    	#	values = {}
+    	#	values["driveid"] = self.driveid
+	 #   	values["driveName"] = self.driveName
+	#	values["country"] = self.country
+	#	values["region"] = self.region
+	#	values["startLat"] = self.startLat
+	#	values["startLon"] = self.startLon
+	#	values["stopLat"] = self.stopLat
+	#	values["stopLon"] = self.stopLon
+	#	values["shortDescription"] = self.shortDescription
+	#	values["longDescription"] = self.longDescription
+	#	values["mileage"] = self.mileage
+	#	values["timeToAllow"] = self.timeToAllow
+	#	values["favorite"] = self.favorite
+	#	values["image"] = self.image
+	#	values["mapimage"] = self.mapimage
+	#	values["restrictions"] = self.restrictions
+	#	values["seasons"] = self.seasons
+	#	values["considerations"] = self.considerations
+	#	values["directions"] = self.directions
+	#	values["imageCredits"] = self.imageCredits
+    	#	return values
 		
 	def getColumns(self):
 		cols = []
@@ -72,7 +72,12 @@ class DBDrive(DBObject):
 		return cols
 
 	def toString(self):
-		print (self.driveid + "," + self.driveName + "," + self.country + "," + self.region + "," + self.startLat + "," + self.startLon + "," + self.stopLat + "," + self.stopLon + "," + self.shortDescription  + "," + self.longDescription + "," + self.mileage + "," + self.timeToAllow + "," + self.favorite + "," + self.image + "," + self.mapimage + "," + self.restrictions + "," + self.seasons + "," + self.considerations + "," + self.directions + "," + self.imageCredits)
+		#print (self.driveid + "," + self.driveName + "," + self.country + "," + self.region + "," + self.startLat + "," + self.startLon + "," + self.stopLat + "," + self.stopLon + "," + self.shortDescription  + "," + self.longDescription + "," + self.mileage + "," + self.timeToAllow + "," + self.favorite + "," + self.image + "," + self.mapimage + "," + self.restrictions + "," + self.seasons + "," + self.considerations + "," + self.directions + "," + self.imageCredits)
+		valString = ""
+		for col in getColumns():
+			valString = valString + DBObject.m_Values[col] + ","
+		print valString
+		
 		
 	def getTableName(self):
 		return "drives"

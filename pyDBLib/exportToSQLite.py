e25d9ee10eb5e2eab915f90841c341e4f09f7f52
Chris <chris@csfweb.net>
2013-05-14 07:40:25 -0300

    Update exportToSQLite.py

--- pyDBLib/exportToSQLite.py
+++ pyDBLib/exportToSQLite.py
@@ -1,1 +1,43 @@
-test
+#!/usr/bin/python
+# _*_ coding: utf-8 _*_
+
+import sys
+import os
+import MySQLdb
+import sqlite3 as lite
+import sys
+import DBObject
+import urllib2
+from DBDrive import DBDrive
+from DBCoordinate import DBCoordinate
+from DBDrive import DBDrive
+from chrispwd import getUserName
+from chrispwd import getPassword
+
+
+def getMySqlConnection(theDBName):
+  return MySQLdb.Connection(user=getUserName(), passwd=getPassword(), db=theDBName, host='localhost')
+
+con = getMySqlConnection("djangosite")
+#con.autocommit(True);
+
+try:
+  cur = con.cursor(MySQLdb.cursors.DictCursor)
+  cur.execute("SELECT * FROM bywayexplorer_drive")
+  driveRows = cur.fetchall()
+  
+  liteCon = lite.connect('BywayExplorer.db')
+  liteCur = liteCon.cursor()
+  tempDrive = DBDrive()
+  tempDrive.createSQLiteTable("BywayExplorer.db",true)
+  
+  for driveRow in driveRows:
+    drive = DBDrive(driveRow)
+    drive.printValString()
+  
+except MySQLdb.Error, e:
+  print "Error %d: %s" % (e.args[0],e.args[1])
+  sys.exit(1)
+finally:
+  if con:
+    con.close() 

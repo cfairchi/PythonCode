9cb9d5010a70d5b9606e16da824cbc083fa5d91f
Chris Fairchild <chris@csfweb.net>
2013-05-18 08:54:52 -0400

    updated paths

--- streetView/processKML.py
+++ streetView/processKML.py
@@ -6,9 +6,9 @@ for root, dirs, files in os.walk("/var/www/staticfiles/kml/"):
     for fName in files:
         if fName.endswith(".kml"):
         	outFile = fName.replace(".kml","").replace("kml_","video_") + ".mpeg"
-        	if (not os.path.exists("/var/www/staticfiles/video/" + outfile)):
+        	if (not os.path.exists("/var/www/staticfiles/video/" + outFile)):
         		print (fname)
-        		print ("/var/www/staticfiles/video/" + outfile)
+        		print ("/var/www/staticfiles/video/" + outFile)
         		kmlFile = os.path.join(fName)
             	#lines = open(kmlFile).read()
             	#generateVideoFromKML(lines,fName)

5e69f6a38d6bd278408cf067a740982e59983f4f
Chris <chris@csfweb.net>
2013-05-22 06:36:39 -0400

    Update process.py

--- imageConvert/process.py
+++ imageConvert/process.py
@@ -1,27 +1,10 @@
 import os
+import Image
 
-from csf_StreetView import generateVideoFromKML
 
-requestCount = 0
-
-for root, dirs, files in os.walk("/var/www/staticfiles/kml/"):
+for root, dirs, files in os.walk("~/git-repos/PythonCode/imageConvert/"):
     for fName in files:
-        if fName.endswith(".kml"):
-        	outFile = fName.replace(".kml","").replace("kml_","video_") + ".avi"
-                checkFile = "/var/www/staticfiles/video/" + outFile
-		
-		print ("Checking for " + checkFile)
-        	if (not os.path.exists(checkFile)):
-			if requestCount >23000:
-				print ("Aborting RequestCount=" + requestCount)
-				break
-			print("Processing " + outFile)
-        		kmlFile = os.path.join(fName)
-        		kmlFile = "/var/www/staticfiles/kml/" + kmlFile
-        		#print (kmlFile)
-			#print ("/var/www/staticfiles/video/" + outFile)
-            		lines = open(kmlFile).read()
-            		requestCount += generateVideoFromKML(lines,outFile)
-
+        if fName.endswith(".gif"):
+		print fName
 
 

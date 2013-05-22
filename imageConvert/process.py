939e8d7f5ba8e615af09b4ae6fc08383b3c08eeb
Chris <chris@csfweb.nett>
2013-05-22 06:34:45 -0400

    add image dir

---
+++ imageConvert/process.py
@@ -0,0 +1,27 @@
+import os
+
+from csf_StreetView import generateVideoFromKML
+
+requestCount = 0
+
+for root, dirs, files in os.walk("/var/www/staticfiles/kml/"):
+    for fName in files:
+        if fName.endswith(".kml"):
+        	outFile = fName.replace(".kml","").replace("kml_","video_") + ".avi"
+                checkFile = "/var/www/staticfiles/video/" + outFile
+		
+		print ("Checking for " + checkFile)
+        	if (not os.path.exists(checkFile)):
+			if requestCount >23000:
+				print ("Aborting RequestCount=" + requestCount)
+				break
+			print("Processing " + outFile)
+        		kmlFile = os.path.join(fName)
+        		kmlFile = "/var/www/staticfiles/kml/" + kmlFile
+        		#print (kmlFile)
+			#print ("/var/www/staticfiles/video/" + outFile)
+            		lines = open(kmlFile).read()
+            		requestCount += generateVideoFromKML(lines,outFile)
+
+
+

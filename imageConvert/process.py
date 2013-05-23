c066c6c71c7b2314812c7ca2091dc6dcccf78d2b
Chris <chris@csfweb.net>
2013-05-23 08:14:12 -0300

    Update process.py

--- imageConvert/process.py
+++ imageConvert/process.py
@@ -5,16 +5,31 @@
 import os
 import Image
 
+#basewidth = new width of image, example 300
+def resizeImage(path, basewidth):
+   import PIL
+   from PIL import Image
 
-for root, dirs, files in os.walk("."):
-    for fName in files:
-        if fName.endswith(".png"):
+   img = Image.open(path)
+   wpercent = (basewidth/float(img.size[0]))
+   hsize = int((float(img.size[1])*float(wpercent)))
+   img = img.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
+   path = path.replace(".jpg","_300.jpg")
+   img.save(path)
+   
+def convert(fName):
+	if fName.endswith(".png"):
 		im = Image.open(fName)
 		if im.mode != "RGB":
 			im = im.convert("RGB")
-		newName = fName.replace(".png",".jpg")
-		print("replacing " + fName + " with " + newName)
-		im.save(newName,"JPEG")
+			newName = fName.replace(".png",".jpg")
+			print("replacing " + fName + " with " + newName)
+			im.save(newName,"JPEG")
+		
+for root, dirs, files in os.walk("."):
+    for fName in files:
+        if fName.endswith(".jpg"):
+		resizeImage(path, 300)
 		
 
 

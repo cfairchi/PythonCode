98d51cd616b813e431a34ee7a17b4bc07ea4c076
Chris <chris@csfweb.net>
2013-06-19 12:37:27 -0300

    Update insertText.py

--- fileUtils/insertText.py
+++ fileUtils/insertText.py
@@ -7,7 +7,7 @@ def insertText(directory, extension, text):
           os.rename( fName, fName+"~" )
           f = open(fName,"w+")
           f.write(text)
-          lines = open(kmlFile).read()
+          lines = open(fName+"~").read()
           for line in Lines:
             f.write(line)
           f.Close  

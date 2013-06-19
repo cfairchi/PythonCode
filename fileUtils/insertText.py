cf9e5b3af5fcc7d865aea146e4bd14b3ef1d9a87
Chris <chris@csfweb.net>
2013-06-19 12:33:10 -0300

    Update insertText.py

--- fileUtils/insertText.py
+++ fileUtils/insertText.py
@@ -1,1 +1,15 @@
-afdgasdfgsdgf
+import os
+
+insertText(".", ".cs" ,"//*****************************************************************");
+
+def insertText(directory, extension, text):
+  for root, dirs, files in os.walk(directory):
+    for fName in files:
+        if fName.endswith(extension):
+          os.rename( fName, fName+"~" )
+          f = open(fName,"w+")
+          f.write(text)
+          lines = open(kmlFile).read()
+          for line in Lines:
+            f.write line
+          f.Close  
